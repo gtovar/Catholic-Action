110637b474fc5f6009eb01e065ab78975110391f
gtovar1985 <gtovar@incaztech.com>
2014-06-16 15:13:23 -0500

    Inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CatholicAction::Application.config.secret_key_base = '8dbf07982d2769e96c669e93ec6ef1a0b86fda9ce41c3e0139446d7ad22f1911c945e81b3fb2e81721e21e22bf7c933abcefc6495749c93a6c2d4e6f58801f73'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CatholicAction::Application.config.session_store :cookie_store, key: '_catholic_action_session'
