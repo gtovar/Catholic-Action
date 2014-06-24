b1abf5281ee51eda6c601ad83dcd767510a77945
gtovar1985 <gtovar@incaztech.com>
2014-06-23 23:31:10 -0500

    Tree structure IS READY

--- Gemfile
+++ Gemfile
@@ -45,3 +45,4 @@ end
 # gem 'debugger', group: [:development, :test]
 
 gem 'xray-rails', group: :development
+gem 'ancestry'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 CatholicAction::Application.routes.draw do
+  resources :pages
+
   root "static_pages#home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
